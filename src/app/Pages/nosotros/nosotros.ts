5e28067a1fed5505bc6fa90eb280929ea4846532
chalon30 <angelfabiano3030@gmail.com>
2025-06-21 19:24:10 -0500

    avance v1.6

---
+++ src/app/Pages/nosotros/nosotros.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-nosotros',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
+  templateUrl: './nosotros.html',
+  styleUrls: ['./nosotros.css']
+})
+export class Nosotros {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Principal } from './Pages/principal/principal';
+import { Nosotros } from './Pages/nosotros/nosotros';
 
 
 export const routes: Routes = [
-    { path: '', component: Principal }
+  { path: '', component: Principal },
+  { path: 'nosotros', component: Nosotros },
 ];
