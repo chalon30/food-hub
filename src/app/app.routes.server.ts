d45d2f29a0fcfea1f983365ff7585a2596923580
chalon30 <angelfabiano3030@gmail.com>
2025-06-21 20:17:01 -0500

    avance v2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'producto/:id',
+    renderMode: RenderMode.Client // 👈 se renderiza en el cliente
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // 👈 el resto sí se prerenderiza
   }
 ];
