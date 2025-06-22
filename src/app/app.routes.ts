e331b4ef3af58def64e64b8d4d085742ba8602b9
chalon30 <angelfabiano3030@gmail.com>
2025-06-21 20:09:36 -0500

    avance v1.9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { Principal } from './Pages/principal/principal';
 import { Nosotros } from './Pages/nosotros/nosotros';
-import { DetalleProductoComponent } from './Pages/detalle-producto/detalle-producto'; // <-- Importa el componente
+import { DetalleProductoComponent } from './Pages/detalle-producto/detalle-producto';
 
 export const routes: Routes = [
   { path: '', component: Principal },
   { path: 'nosotros', component: Nosotros },
-  { path: 'producto/:id', component: DetalleProductoComponent },
+  {
+    path: 'producto/:id',
+    component: DetalleProductoComponent,
+    data: { renderMode: 'client' }
+  }
 ];
